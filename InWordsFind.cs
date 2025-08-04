d1a01f8891321f5278b3e8461487adb8763549ca
Elshod <Elshod@DESKTOP-EVQNTO7>
2025-08-04 12:17:20 +0300

    InWordsFind.cs yani matnlar ichidan  me  degan suz bormi deyapti

---
+++ InWordsFind.cs
@@ -0,0 +1,10 @@
+﻿namespace Algoritm
+{
+    public partial class InWordsFind
+    {
+        public static string[] SearchWords(string query, string[] words)
+        => QueryFind(query, words);
+
+        
+    }
+}
