f640ee639a6a29db7d25ccc2c1ad28f38070f8c3
Elshod <Elshod@DESKTOP-EVQNTO7>
2025-08-04 12:01:28 +0300

    inson it va kuchuk oldi va yoshlarni hisoblash

---
+++ HumanTakeCatandDog.cs
@@ -0,0 +1,14 @@
+﻿namespace Algoritm
+{
+  public class HumantakeCatandDog
+    {  public static int[] humanYearsCatYearsDogYears(int humanYears)
+        {
+            int catYears = humanYears==1 ? 15 :
+                 humanYears==2 ? 24 : 24+(humanYears-2)*4;
+            int dogYears = humanYears==1 ? 15 :
+                humanYears==2 ? 24 : 24+(humanYears-2)*5;
+
+            return new int[] { humanYears, catYears, dogYears };
+        }
+    }
+}
