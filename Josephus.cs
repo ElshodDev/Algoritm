f20959edb63c76fc20908c3642d86d69b43ef53c
Elshod <Elshod@DESKTOP-EVQNTO7>
2025-08-04 12:23:21 +0300

    josephus da qadam bilan teskari qilish

---
+++ Josephus.cs
@@ -0,0 +1,18 @@
+﻿namespace Algoritm
+{
+    public class Josephus
+    { 
+        public static List<object> JosephusPermutation(List<object> items, int k)
+        {
+           List<object> result = new List<object>();
+            int index = 0;
+            while (items.Count>0) 
+            {
+                index=(index+k-1)%items.Count;
+                result.Add(items[index]);
+                items.RemoveAt(index);
+            }
+            return result;
+        }
+    }
+}
