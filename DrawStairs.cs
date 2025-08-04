0148a1a5694545cc919aeb64ed1cf3c91f4ba161
Elshod <Elshod@DESKTOP-EVQNTO7>
2025-08-04 12:40:27 +0300

    zinapoya yasash

---
+++ DrawStairs.cs
@@ -0,0 +1,16 @@
+﻿namespace Algoritm
+{
+    public class DrawStairs
+    {
+        public static string DrawStairFunc(int stairHeight)
+        {
+            string stairs=string.Empty;
+            char space = ' ';
+            for (int iteration = 1; iteration < stairHeight; iteration++) 
+            {
+                stairs +="I\n"+new String(space, count: iteration);
+            }
+            return stairs+"I";
+        }
+    }
+}
