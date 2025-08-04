c3b5b0cb4fcf1b1f85daa3ba43b8d31e43699e81
Elshod <Elshod@DESKTOP-EVQNTO7>
2025-08-04 12:48:45 +0300

    massivdagi raqma va nollar aralash berilganda ularni nollarni oxidan joylash

---
+++ ZeroFromEnd.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.Design;
+
+namespace Algoritm
+{
+    public class ZeroFromEnd
+    {
+     public static int[] MoveZeros(int[] arr)
+        {
+            List<int> result = new List<int>();
+            int zeroCount = 0;
+            foreach (int num in arr) 
+            {
+                if(num == 0)
+                    zeroCount++;
+                else
+                    result.Add(num);
+            }
+            for(int i=0; i<zeroCount; i++)
+            {
+                result.Add(0);
+            }
+            return result.ToArray();
+        }
+    }
+}
