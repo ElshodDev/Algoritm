2834b89e9b6359cf17eadcf50f1380870720a71f
Elshod <Elshod@DESKTOP-EVQNTO7>
2025-08-04 12:49:27 +0300

    umumiy baxo hisoblash

---
+++ finalGrade.cs
@@ -0,0 +1,14 @@
+﻿namespace Algoritm
+{
+    public class finalGrade
+    {
+        public static int FinalGrade(int examGrade, int countProject)
+        {
+            return
+            examGrade>90 | countProject > 10 ? 100 :
+            examGrade>75 | countProject >= 5 ? 90 :
+            examGrade>50 | countProject >= 2 ? 75 :
+            0;
+        }
+    }
+}
