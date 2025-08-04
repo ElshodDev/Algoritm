d82ae2052f48d12c5f09c2c6aad1f71833efe2d5
Elshod <Elshod@DESKTOP-EVQNTO7>
2025-08-04 12:45:37 +0300

    alif bulsa fila and alif\nfid bulsa nfid\lif qilish

---
+++ Opstrings.cs
@@ -0,0 +1,23 @@
+﻿namespace Algoritm
+{
+    public class Opstrings
+    {
+        public static string VertMirror(string strng)
+        {
+            string[] lines = strng.Split('\n');
+            var reversedlines=lines.Select(line=>
+            new string(line.Reverse().ToArray().ToArray()));
+            return string.Join("\n", reversedlines);
+        }
+        public static string HorMirror(string strng)
+        {
+            string[] goriz=strng.Split("\n").Reverse().ToArray();
+            return string.Join("\n", goriz);
+
+        }
+        public static string Oper(Func<string, string> fct, string s)
+        {
+            return fct(s);
+        }
+    }
+}
