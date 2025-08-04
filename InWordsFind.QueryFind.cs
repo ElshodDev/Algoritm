a058cfcfe759d04898c7208973f1d119c07da421
Elshod <Elshod@DESKTOP-EVQNTO7>
2025-08-04 12:18:01 +0300

    InWordsFind.cs ni partial qilingani yani matnlar ichidan  me  degan suz bormi deyapti

---
+++ InWordsFind.QueryFind.cs
@@ -0,0 +1,23 @@
+﻿namespace Algoritm
+{
+    public partial class InWordsFind
+    {
+        private static string[] QueryFind(string query, string[] words)
+        {
+            List<string> result = new List<string>();
+
+            foreach (string word in words)
+            {
+                if (word.ToLower().Contains(query.ToLower()))
+                {
+                    result.Add(word);
+                }
+            }
+            if (result.Count == 0)
+            {
+                return new string[] { "Empty" };
+            }
+            return result.ToArray();
+        }
+    }
+}
