48e085cd1fba5a14c263371225b7b77b5e4e1340
Elshod <Elshod@DESKTOP-EVQNTO7>
2025-08-04 11:57:45 +0300

    CenturyFromYears class

---
+++ CenturyFromYear.cs
@@ -0,0 +1,14 @@
+﻿namespace Algoritm
+{
+    public class CenturyFromYear
+    {
+        public static int calculator(int year)
+        {
+            if (year %100==0)
+            {
+                return year /100;
+            }
+            return year/100+1;
+        }
+    }
+}
