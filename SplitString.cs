39a7c6ea641b6381f477fe9f8ec46aaa7fefc3de
Elshod <Elshod@DESKTOP-EVQNTO7>
2025-08-04 12:47:25 +0300

    agr suzlar dagi belgilar juft bulmasa _ qushish  asddd 2 ta dan ajratish as dd d_ kurinishda buladi

---
+++ SplitString.cs
@@ -0,0 +1,22 @@
+﻿namespace Algoritm
+{
+    public class SplitString
+    {
+        public static List<string> Solution(string str)
+        {
+            if (str.Length%2!=0)
+            {
+                str+="_";
+            }
+            List<string> result = new List<string>();
+            int i = 0;
+
+            while (i < str.Length) 
+            {
+                result.Add(str.Substring(i,2));
+                i += 2;
+            }
+            return result;
+        }
+    }
+}
