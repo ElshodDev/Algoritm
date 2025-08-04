dcc41371c4d6ffe165112452ff55f9c687ded536
Elshod <Elshod@DESKTOP-EVQNTO7>
2025-08-04 11:59:34 +0300

    birinchi suzni oxiriga utqazib va yana suz qushish

---
+++ FirstOutEnd.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Algoritm
+{
+    public class FirstOutEnd
+    {
+        public static string PigIt(string str)
+        {
+            string[] strings=str.Split(' ');
+
+            for(int i=0; i<strings.Length; i++)
+            {
+                strings[i]=TransformWord(strings[i]);
+            }
+            return string.Join(" ", strings);
+        }
+
+        private static string TransformWord(string strings)
+        {
+            if (strings.Length <=1) return strings;
+            return strings.Substring(1)+strings[0]+"ay";
+        }
+    }
+}
