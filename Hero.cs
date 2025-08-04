ef424c7912178339dea04c01621aea1234215d4b
Elshod <Elshod@DESKTOP-EVQNTO7>
2025-08-04 12:41:42 +0300

    class and constructor

---
+++ Hero.cs
@@ -0,0 +1,20 @@
+﻿namespace Algoritm
+{
+    public class Hero
+    {
+        public string Name { get; set; } 
+        public string Position { get; set; }
+        public float Health { get; set; }
+        public float Damage { get; set; } 
+        public int Experience { get; set; } 
+        public Hero(string name="Hero")
+        {
+            Name = name;
+            Position = "00";
+            Health = 100;
+            Damage = 5;
+            Experience = 0;
+
+        }
+    }
+}
