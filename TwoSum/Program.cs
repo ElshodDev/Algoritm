dbdc1d54ec79ba12fd9d6b249093b4458f11b499
Elshod <elshodibadullayev28@gmail.com>
2025-10-10 13:58:24 +0500

    TwoSum

---
+++ TwoSum/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Algoritm
+{
+    public class Program
+    {
+        public static int[] twoSum(int[] nums,int target)
+        {
+            for(int i=0; i<nums.Length; i++)
+            {
+                for (int j =i+1; j<nums.Length; j++) 
+                {
+                    if (nums[i]+nums[j] == target)
+                    {
+                        return new int[] {i,j};
+                    }
+                }
+            }
+            return new int[0];
+        }
+        
+    }
+}
