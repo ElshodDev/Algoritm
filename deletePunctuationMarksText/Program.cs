5922575d4fa473f666c5ec91ff26d821a8cde253
Elshod <elshodibadullayev28@gmail.com>
2025-10-11 20:04:29 +0500

    deletePunctuationMarksText

---
+++ deletePunctuationMarksText/Program.cs
@@ -0,0 +1,20 @@
+﻿public class deletePunctuationMarksText
+{
+		static void Main(string[] args)
+		{
+				string text = "Hello, World! This is a test: Does it work? Yes, it does.";
+				char[] punctuationMarks = new char[] { '.', ',', '!', '?', ':', ';', '-', '_', '(', ')', '[', ']', '{', '}', '\'', '\"' };
+
+				string result = RemovePunctuationMarks(text, punctuationMarks);
+				Console.WriteLine(result);
+		}
+
+		static string RemovePunctuationMarks(string input, char[] punctuationMarks)
+		{
+				foreach (char mark in punctuationMarks)
+				{
+						input = input.Replace(mark.ToString(), string.Empty);
+				}
+				return input;
+		}
+}
